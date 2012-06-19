857b3271c6d432b2db6ea663f2953d3b383147d7
Ian Oxley <ijoxley@gmail.com>
2012-06-19 16:42:45 +0100

    Appends the git svn clone command after the svn checkout cmd.
    Adds some CSS to the <head> tag, then grabs the svn command and creates a git svn alternative.

---
+++ gitsvn.user.js
@@ -0,0 +1,30 @@
+// gitsvn.user.js
+// --------------
+
+var cmd = document.getElementById('checkoutcmd');
+if (cmd) {
+  var svn = cmd.innerHTML;
+  if (svn.search(/^\s*svn checkout/i) === 0) {
+    addCss();
+    appendGitSvn(cmd, svn)
+  }
+}
+
+function addCss() {
+  var css = '#gitcmd { display: block; margin:5px 0; }',
+      style = document.createElement('style');
+
+  style.innerHTML = css;
+  document.head.appendChild(style);
+}
+
+
+function appendGitSvn(elem, html) {
+  var gitCmd = html.replace('svn checkout', 'git svn clone'),
+      gitElem = document.createElement('tt'),
+      parent = elem.parentNode;
+
+  gitElem.id = "gitcmd";
+  gitElem.innerHTML = gitCmd;
+  parent.insertBefore(gitElem, elem.nextSibling);
+}
